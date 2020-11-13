e40efe49937f464cd96c8637f46b8253009e5d37
Piper Merriam <pipermerriam@gmail.com>
2020-11-13 08:44:18 -0700

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ with open('./README.md') as readme:
 setup(
     name='upnp-port-forward',
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version='0.1.1',
+    version='0.1.2',
     description="""upnp-port-forward: UPnP port forwarding for humans""",
     long_description=long_description,
     long_description_content_type='text/markdown',
